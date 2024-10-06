4c44826812762fe8de147a93bba69f6406b9b137
Mateus G. Spindula <mgspindula@gmail.com>
2024-10-06 14:19:54 -0300

    Update metatags.js

--- metatags.js
+++ metatags.js
@@ -1,22 +1,24 @@
-var script = document.createElement('script');
-script.type = "application/ld+json";
-script.text = JSON.stringify({
-  "@context": "https:\/\/schema.org",
-  "@type": "Game",
-  "name": "Periculum RPG",
-  "description": "Periculum RPG é um role-playing game inspirado no mundo mágico pós-guerra de Harry Potter, cheio de aventuras e mistérios.",
-  "genre": "RPG",
-  "audience": {
-    "@type": "PeopleAudience",
-    "suggestedMinAge": "14"
-  },
-  "author": {
-    "@type": "Organization",
-    "name": "Periculum RPG"
-  },
-  "publisher": {
-    "@type": "Organization",
-    "name": "Periculum RPG"
-  }
+$(document).ready(function() {
+	var script = document.createElement('script');
+	script.type = "application/ld+json";
+	script.text = JSON.stringify({
+	  "@context": "https:\/\/schema.org",
+	  "@type": "Game",
+	  "name": "Periculum RPG",
+	  "description": "Periculum RPG é um role-playing game inspirado no mundo mágico pós-guerra de Harry Potter, cheio de aventuras e mistérios.",
+	  "genre": "RPG",
+	  "audience": {
+		"@type": "PeopleAudience",
+		"suggestedMinAge": "14"
+	  },
+	  "author": {
+		"@type": "Organization",
+		"name": "Periculum RPG"
+	  },
+	  "publisher": {
+		"@type": "Organization",
+		"name": "Periculum RPG"
+	  }
+	});
+	document.head.appendChild(script);
 });
-document.head.appendChild(script);
